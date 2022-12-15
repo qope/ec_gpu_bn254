55bb64c132bb69034de13162644ed6fbc9e771f5
nuno <qope@example.com>
2022-12-15 21:53:48 +0900

    add outdir

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,11 @@
+use std::env;
+
 use blstrs::Scalar;
 use ec_gpu_gen::SourceBuilder;
 use pairing::bn256::Bn256;
 
 fn main() {
+    env::set_var("OUT_DIR", "outdir");
     let source_builder = SourceBuilder::new().add_fft::<Scalar>();
     ec_gpu_gen::generate(&source_builder);
 }
