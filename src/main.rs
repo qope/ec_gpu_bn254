0048f35694e96bd23f488afdecab6f01f9284fe1
nuno <41233686+qope@users.noreply.github.com>
2022-12-16 19:14:51 +0900

    wip

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use blstrs::Scalar;
 use ec_gpu_gen::SourceBuilder;
-use pairing::bn256::Bn256;
+// use pairing::bn256::Fr as Scalar;
 
 fn main() {
     let source_builder = SourceBuilder::new().add_fft::<Scalar>();
